ce59cb324e8bb50bf2787ca55952bf6d6ce59c3c
teerapat <teerapatpor@gmai.com>
2021-03-02 20:52:09 +0700

    connect database for deploy

--- server/server.php
+++ server/server.php
@@ -1,7 +1,14 @@
 <?php
-$server_name = "localhost";
-$username = "root";
-$password = "";
-$database_name = "project";
+// for develop
+// $server_name = "localhost";
+// $username = "root";
+// $password = "";
+// $database_name = "project";
+
+// for deploy
+$server_name = "remotemysql.com";
+$username = "qBpQoAJ2nV";
+$password = "YdRGrqAUgm";
+$database_name = "qBpQoAJ2nV";
 
 $connect = mysqli_connect($server_name, $username, $password, $database_name);
